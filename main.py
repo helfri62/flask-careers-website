0e5ecd340340c387fa2693a9dcc25dc75ab50502
kahelfrich@gmail.com <kahelfrich@gmail.com>
2024-05-02 22:44:23 -0400

    COmmit from VS committed
    COmmit from VS

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-#main.py
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Flask Ahoy</h1><p>Kurt\'s Web App with Python Flask! Now with a title!</p>'
+
+app.run(host='0.0.0.0', port=81)
